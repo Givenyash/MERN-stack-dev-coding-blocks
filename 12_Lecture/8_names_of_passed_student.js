8880b740236095b58a73477e9440b5b7f9fe7644
Givenyash <yash.ishu.barehi@gmail.com>
2026-06-17 14:17:28 +0530

    Practice Question

---
+++ 12_Lecture/8_names_of_passed_student.js
@@ -0,0 +1,14 @@
+//Q.8
+
+const results = [
+    { name: "A", marks: 35 }, 
+    { name: "B", marks: 75 }, 
+    { name: "C", marks: 45 },
+    { name: "D", marks: 25} 
+]
+
+const passStudents = results
+.filter(num =>num.marks >= 40)
+.map(num => num.name);
+
+console.log(passStudents);
