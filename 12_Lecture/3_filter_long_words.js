a5ca2261c0a4b9ad5b28caba0b1a9dae8cc28662
Givenyash <yash.ishu.barehi@gmail.com>
2026-06-17 14:16:24 +0530

    Practice Questions

---
+++ 12_Lecture/3_filter_long_words.js
@@ -0,0 +1,7 @@
+//Q.3 Filter Long words
+//keep words havong length greater than 5.
+
+const words = ["apple","banana","cat","elephant","dog"];
+const animals = words.filter(word => word.length > 5);
+
+console.log(animals);
