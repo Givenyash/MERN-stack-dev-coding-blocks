dcd325c50113a8eb7bd7d080cdb7ebc1e0692bf0
Givenyash <yash.ishu.barehi@gmail.com>
2026-06-18 12:48:11 +0530

    Practice Question | Group products by category

---
+++ 12_Lecture/12_group_products_by_category.js
@@ -0,0 +1,17 @@
+//Q.12    group products by category
+
+const products = [
+    {name : "Laptop", category : "Electronics"},
+    {name : "Phone", category : "Electronics"},
+    {name : "Shirt", category : "Clothing"}
+];
+
+const groupProducts = products.reduce(function(group,product){
+    if(!group[product.category]){
+        group[product.category] = [];
+    }
+    group[product.category].push(product.name);
+    return group
+},{});
+
+console.log(groupProducts);
